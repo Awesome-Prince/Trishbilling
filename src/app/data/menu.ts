0f7acc71582907d61fd2544c5d66a889a1118864
「 LovelyPrince™ 」 <harishk2appu@gmail.com>
2026-04-06 22:48:06 +0530

    Update menu.ts

--- src/app/data/menu.ts
+++ src/app/data/menu.ts
@@ -5,147 +5,476 @@ import { MenuItem } from "../types";
 // Simply change the name, price, or add new items to the array
 
 export const MENU_ITEMS: MenuItem[] = [
-  //  Hot Beverages
+  // Hot Beverages
   {
     id: "1",
-    name: "Espresso",
-    price: 50,
+    name: "Hot chocolate",
+    price: 70,
     category: "Hot Beverages",
   },
   {
     id: "2",
-    name: "Cappuccino",
-    price: 80,
-    category: "Hot Beverages",
+    name: "Hot milo",
+    price: 70,
+    category: "Hot Beve rages",
   },
   {
     id: "3",
-    name: "Latte",
-    price: 90,
+    name: "Hot boost",
+    price: 50,
     category: "Hot Beverages",
   },
+
+  // Cold Beverages
   {
     id: "4",
-    name: "Hot Chocolate",
-    price: 100,
-    category: "Hot Beverages",
+    name: "Cold Coffee",
+    price: 80,
+    category: "Cold Beverages",
   },
   {
     id: "5",
-    name: "Masala Chai",
-    price: 40,
-    category: "Hot Beverages",
+    name: "Cold milo",
+    price: 50,
+    category: "Cold Beverages",
   },
-
-  // 🥤 Cold Beverages
   {
     id: "6",
-    name: "Iced Coffee",
-    price: 100,
+    name: "Cold boost",
+    price: 50,
     category: "Cold Beverages",
   },
+
+  // Mojito
   {
     id: "7",
-    name: "Cold Coffee",
-    price: 110,
-    category: "Cold Beverages",
+    name: "Lemon mint mojito",
+    price: 50,
+    category: "Mojito",
   },
   {
     id: "8",
-    name: "Iced Tea",
+    name: "Lichi mojito",
     price: 70,
-    category: "Cold Beverages",
+    category: "Mojito",
   },
   {
     id: "9",
-    name: "Fresh Lime Soda",
-    price: 60,
-    category: "Cold Beverages",
+    name: "Blackberrry mojito",
+    price: 70,
+    category: "Mojito",
   },
   {
     id: "10",
-    name: "Mango Shake",
-    price: 120,
-    category: "Cold Beverages",
+    name: "Green apple mojito",
+    price: 70,
+    category: "Mojito",
   },
-
-  // 🥪 Food Items
   {
     id: "11",
-    name: "Veg Sandwich",
-    price: 120,
-    category: "Food Items",
+    name: "Strawberry mojito",
+    price: 70,
+    category: "Mojito",
   },
+
+  // MilkShake
   {
     id: "12",
-    name: "Cheese Sandwich",
-    price: 140,
-    category: "Food Items",
+    name: "Chocolate milkshake",
+    price: 70,
+    category: "MilkShake",
   },
   {
     id: "13",
-    name: "Paneer Sandwich",
-    price: 160,
-    category: "Food Items",
+    name: "Orio milkshake",
+    price: 90,
+    category: "MilkShake",
   },
   {
     id: "14",
-    name: "French Fries",
-    price: 90,
-    category: "Food Items",
+    name: "Kitkat milkshake",
+    price: 100,
+    category: "MilkShake",
   },
   {
     id: "15",
-    name: "Veg Burger",
-    price: 130,
-    category: "Food Items",
+    name: "Vanilla milkshake",
+    price: 70,
+    category: "MilkShake",
   },
   {
     id: "16",
-    name: "Pizza (Small)",
-    price: 200,
-    category: "Food Items",
+    name: "Stawberry milkshake",
+    price: 80,
+    category: "MilkShake",
   },
   {
     id: "17",
-    name: "Pasta",
-    price: 180,
-    category: "Food Items",
+    name: "Butterscotch milkshake",
+    price: 80,
+    category: "MilkShake",
   },
-
-  // 🍰 Snacks & Desserts
   {
     id: "18",
-    name: "Samosa",
-    price: 25,
-    category: "Snacks & Desserts",
+    name: "Blackcurrent milkshake",
+    price: 90,
+    category: "MilkShake",
   },
-  {
+
+// ThickShake
+{
     id: "19",
-    name: "Pakora Plate",
-    price: 80,
-    category: "Snacks & Desserts",
+    name: "Chocolate thickshake",
+    price: 100,
+    category: "ThickShake",
   },
   {
     id: "20",
-    name: "Brownie",
-    price: 110,
-    category: "Snacks & Desserts",
+    name: "Strawberry Thickshake",
+    price: 100,
+    category: "ThickShake",
   },
   {
     id: "21",
-    name: "Cheesecake",
-    price: 150,
-    category: "Snacks & Desserts",
+    name: "Butterscotch Thickshake",
+    price: 120,
+    category: "ThickShake",
   },
   {
     id: "22",
-    name: "Muffin",
+    name: "Blackcurrent Thickshake",
+    price: 120,
+    category: "ThickShake",
+  },
+  {
+    id: "23",
+    name: "Boost with dates",
+    price: 150,
+    category: "ThickShake",
+  },
+  
+// Panipuri
+  {
+    id: "24",
+    name: "Panipuri",
+    price: 20,
+    category: "Panipuri",
+  },
+  {
+    id: "25",
+    name: "Dahi puri",
+    price: 40,
+    category: "Panipuri",
+  },
+  {
+    id: "26",
+    name: "Chesse puri",
+    price: 50,
+    category: "Panipuri",
+  },
+  {
+    id: "27",
+    name: "Mayo puri",
+    price: 40,
+    category: "Panipuri",
+  },
+  {
+    id: "28",
+    name: "Sweet puri",
+    price: 30,
+    category: "Panipuri",
+  },
+  {
+    id: "29",
+    name: "Shawarma puri",
+    price: 50,
+    category: "Panipuri",
+  },
+  {
+    id: "30",
+    name: "BBQ Dry puri",
+    price: 40,
+    category: "Panipuri",
+  },
+
+// Quickbites
+{
+    id: "31",
+    name: "French fries",
+    price: 50,
+    category: "Quickbites",
+  },
+  {
+    id: "32",
+    name: "Peri Peri fries",
+    price: 80,
+    category: "Quickbites",
+  },
+  {
+    id: "33",
+    name: "Crispy Chicken popcorn",
+    price: 100,
+    category: "Quickbites",
+  },
+  {
+    id: "34",
+    name: "Crispy Chicken pakoda",
+    price: 100,
+    category: "Quickbites",
+  },
+  {
+    id: "35",
+    name: "Chicken samosa",
+    price: 20,
+    category: "Quickbites",
+  },
+  {
+    id: "36",
+    name: "Chicken Cheesy strips",
+    price: 120,
+    category: "Quickbites",
+  },
+  {
+    id: "37",
+    name: "Chicken nuggets",
+    price: 100,
+  },
+
+  // Sandwich
+ {
+    id: "38",
+    name: "Veg Cheese toast",
+    price: 60,
+    category: "Sandwich",
+  },
+  {
+    id: "39",
+    name: "Chicken Cheese toast",
+    price: 100,
+    category: "Sandwich",
+  },
+  {
+    id: "40",
+    name: "Paneer Cheese toast",
+    price: 100,
+    category: "Sandwich",
+  },
+  {
+    id: "41",
+    name: "Egg scrambled cheese toast",
+    price: 80,
+    category: "Sandwich",
+  },
+  {
+    id: "42",
+    name: "Bread omlet",
+    price: 50,
+    category: "Sandwich",
+  },
+  {
+    id: "43",
+    name: "Murukku sandwich",
+    price: 50,
+    category: "Sandwich",
+  },
+  {
+    id: "44",
+    name: "Murukku chesse sandwich",
     price: 70,
-    category: "Snacks & Desserts",
+    category: "Sandwich",
+  },
+  
+
+// Burger
+ {
+    id: "45",
+    name: "Crispy chicken ginger",
+    price: 100,
+    category: "Burger",
+  },
+  {
+    id: "46",
+    name: "Paneer jalapeno",
+    price: 100,
+    category: "Burger",
+  },
+  {
+    id: "47",
+    name: "House special twin tower",
+    price: 150,
+    category: "Burger",
+  },
+  {
+    id: "48",
+    name: "Egg cheese",
+    price: 80,
+    category: "Burger",
+  },
+  {
+    id: "49",
+    name: "Classic chicken",
+    price: 80,
+    category: "Burger",
+  },
+
+  // Momos
+  {
+    id: "50",
+    name: "Veg steam",
+    price: 50,
+    category: "Momos",
+  },
+  {
+    id: "51",
+    name: "Paneer steam",
+    price: 80,
+    category: "Momos",
+  },  
+ {
+    id: "52",
+    name: "Corn cheese steam",
+    price: 100,
+    category: "Momos",
+  },
+  {
+    id: "53",
+    name: "Chicken steam",
+    price: 80,
+    category: "Momos",
+  },
+  {
+    id: "54",
+    name: "Veg schezwan fry",
+    price: 80,
+    category: "Momos",
+  },
+  {
+    id: "55",
+    name: "Paneer schezwan fry",
+    price: 100,
+    category: "Momos",
+  },
+  {
+    id: "56",
+    name: "Corn cheese schezwan fry",
+    price: 130,
+    category: "Momos",
+  },
+  {
+    id: "57"
+    name: "Chicken schezwan fry",
+    price: 130,
+    category: "Momos",
+  }, 
+
+  // Rolls&Shawarma
+  {
+    id: "58",
+    name: "Mexian Shawarma",
+    price: 130,
+    category: "Rolls&Shawarma",
+  },
+  {
+    id: "59",
+    name: "Arabian shawarma",
+    price: 130,
+    category: "Rolls&Shawarma",
+  },  
+ {
+    id: "60",
+    name: "Chicken kati roll",
+    price: 120,
+    category: "Rolls&Shawarma",
+  },
+  {
+    id: "61",
+    name: "Paneer kati roll",
+    price: 120,
+    category: "Rolls&Shawarma",
+  },
+  {
+    id: "62",
+    name: "Prawn kati roll",
+    price: 160,
+    category: "Rolls&Shawarma",
+  },
+  {
+    id: "63",
+    name: "Plain egg masala roll",
+    price: 50,
+    category: "Rolls&Shawarma",
+  },
+  {
+    id: "64",
+    name: "Crispy chicken wraps",
+    price: 100,
+    category: "Rolls&Shawarma",
+  },
+  {
+    id: "65"
+    name: "Paneer wraps",
+    price: 100,
+    category: "Rolls&Shawarma",
+  },
+{
+    id: "66"
+    name: "Egg Cheese wraps",
+    price: 100,
+    category: "Rolls&Shawarma",
+  }, 
+
+  // Cheeseloadedbox
+  {
+    id: "67"
+    name: "Cheeseloaded fries",
+    price: 150,
+    category: "Cheeseloadedbox",
+  },
+  {
+    id: "68"
+    name: "Chessey chicken fries",
+    price: 200,
+    category: "Cheeseloadedbox",
+  },
+  {
+    id: "69"
+    name: "Chicken lass Mexican chicken",
+    price: 200,
+    category: "Cheeseloadedbox",
+  },{
+    id: "70"
+    name: "Chessy cripsy hot wigs",
+    price: 200,
+    category: "Cheeseloadedbox",
+  },
+  {
+    id: "71"
+    name: "Mini cheese lass fries",
+    price: 120,
+    category: "Cheeseloadedbox",
+  },
+
+// Musttry
+    {
+    id: "72"
+    name: "Thread chicken*",
+    price: 60,
+    category: "Musttry",
+  },{
+    id: "73"
+    name: "Hot BBQ leg fry",
+    price: 70,
+    category: "Musttry",
+  },
+  {
+    id: "74"
+    name: "Crispy chicken cheesy fries",
+    price: 200,
+    category: "Musttry",
   },
 ];
 
 export const CATEGORIES = Array.from(
   new Set(MENU_ITEMS.map((item) => item.category))
-);
+); 
+  
